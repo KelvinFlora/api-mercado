6ad9d8df09a7febb63278589d731f711e5e9a060
Kelvin Flora <kelvin.flora@gmail.com>
2023-07-06 18:14:19 -0300

    prop models edited

--- models/Client.js
+++ models/Client.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const ClientInfo = new mongoose.Schema ({
     name: {type: String, required: true},
     cpf: {type: String, required: true},
-    adress: {type: String, required: true}
+    phone: {type: String, required: true}
 })
 
 const ClientModel = mongoose.model("clientes", ClientInfo )
--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema({
-  
+    amount: { type: Number, required: true}
 });
 
 const OrderModel = mongoose.model("pedidos", OrderSchema);
