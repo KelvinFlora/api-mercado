784f605beae711456cc1ea6c135744638b9876d8
Kelvin Flora <kelvin.flora@gmail.com>
2023-07-07 19:15:56 -0300

    schema changes

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,8 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const OrderSchema = new mongoose.Schema({
+    clientid: { type: Schema.Types.ObjectId, ref: "clientes"},
+    productnumber: [{ type: Schema.Types.Number, ref: "produtos"}],
     amount: { type: Number, required: true}
 });
 
--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema ({
+    productnumber: { type: Number, required: true},
     name: { type: String, required: true},
     price: { type: Number, required: true}
 })
