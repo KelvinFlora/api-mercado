d45d6d864e7050475109eead57da1cd61a118b8c
Kelvin Flora <kelvin.flora@gmail.com>
2023-07-07 19:16:09 -0300

    res message edited

--- controller/orderController.js
+++ controller/orderController.js
@@ -3,7 +3,7 @@ import OrderModel from "../models/Order.js";
 class OrderController {
   static listOrder = async (req, res) => {
     try {
-      const result = await OrderModel.find();
+      const result = await OrderModel.find().populate("productnumber").exec();
       if (result.length > 0) {
         res.status(200).json(result);
       } else {
@@ -33,22 +33,18 @@ class OrderController {
 
   static registerOrder = async (req, res) => {
     const data = req.body;
-  
-    const entry = new Date();
     try {
-      data.entrytime = entry;
       const result = await OrderModel.create(data);
       if (result) {
         res.status(201).json(result);
       } else {
         res
           .status(400)
-          .json({ message: "Não foi possível registrar a entrada!" });
+          .json({ message: "Não foi possível registrar o pedido!" });
       }
     } catch (error) {
       res.status(500).json({
-        message: "Erro interno do servidor.",
-        error: process.env.DEVELOPMENT ? error : undefined,
+        message: "Erro interno do servidor."
       });
     }
   };
@@ -72,45 +68,16 @@ class OrderController {
     }
   };
 
-  static updateOrderExit = async (req, res) => {
-    const id = req.params.id;
-    const parkingEntry = await OrderModel.findById(id);
-    const entry = parkingEntry.entrytime;
-    try {
-      const price = 0.16;
-      const exit = new Date();
-      const timeDiff = exit.getTime() - entry.getTime();
-      const payment = (timeDiff / 60000) * price;
-      const resultExit = await OrderModel.findByIdAndUpdate(
-        id,
-        { exittime: exit, totalPrice: payment },
-        { new: true }
-      );
-
-      if (resultExit) {
-        res.status(200).json({
-          message: `O valor a ser pago é de: ${payment} reais.`,
-        });
-      } else {
-        res
-          .status(404)
-          .json({ message: "Não foi possível atualizar as informações!" });
-      }
-    } catch (error) {
-      res.status(500).json({ message: `Erro interno do servidor!${error}` });
-    }
-  };
-
   static deleteOrder = async (req, res) => {
     const id = req.params.id;
     try {
       const result = await OrderModel.deleteOne({ _id: id });
       if (result.deletedCount > 0) {
-        res.status(200).json(result);
+        res.status(200).json({ message: "Pedido removido com sucesso!"});
       } else {
         res
           .status(404)
-          .json({ message: "Não foi possível excluir a informação!" });
+          .json({ message: "Não foi possível excluir o pedido!" });
       }
     } catch (error) {
       res.status(500).json({ message: "Erro interno do servidor." });
